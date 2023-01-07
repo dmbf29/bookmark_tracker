77f732dae07564870f37b6f371f14c578598e1b7
Douglas Berkley <douglasmberkley@gmail.com>
2023-01-07 15:37:28 +0900

    added validations and associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,15 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
+  has_many :bookmark_tags
+  has_many :tags, through: :bookmark_tags
+  validate :name_or_url
+
+  private
+
+  def name_or_url
+    return unless name.blank? && url.blank?
+
+    errors.add(:base, 'A name and/or URL is required.')
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ Bookmark.destroy_all
 
 user = User.find_by(email: 'douglasmberkley@gmail.com')
 user = User.create!(email: 'douglasmberkley@gmail.com', password: ENV['ADMIN_PASSWORD']) unless user
-bookmark = Bookmark.create!(url: 'http://www.google.com', name: 'Google', user: User)
+bookmark = Bookmark.create!(url: 'http://www.google.com', name: 'Google', user: user)
