d85631095c012cfee93d931f17d7cc86d1ad36b7
Douglas Berkley <douglasmberkley@gmail.com>
2023-01-07 20:30:01 +0900

    updated validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
-  has_many :bookmark_tags
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_tags, through: :bookmarks
   has_many :tags, through: :bookmark_tags
   validate :name_or_url
   accepts_nested_attributes_for :bookmark_tags, allow_destroy: true
@@ -12,5 +13,4 @@ class Bookmark < ApplicationRecord
 
     errors.add(:base, 'A name and/or URL is required.')
   end
-
 end
--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -1,8 +1,11 @@
 class BookmarkPolicy < ApplicationPolicy
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      current_user.bookmarks
+    end
+  end
+
+  def create?
+    true
   end
 end
