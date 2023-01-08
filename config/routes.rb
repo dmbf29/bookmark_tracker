941b4382b81fe0c7b1353f0dd03263c2cd547057
Douglas Berkley <douglasmberkley@gmail.com>
2023-01-08 21:24:36 +0900

    added index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :bookmarks, only: :index
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :bookmarks, only: [ :index, :create ]
