bbf2f85b65cb9e8168e88cef65bb5774db46106d
Douglas Berkley <douglasmberkley@gmail.com>
2023-01-08 21:21:05 +0900

    Added bookmarks index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   include Pundit::Authorization
+  include JsonWebToken
+  attr_reader :current_user
 
+  before_action :authenticate_request, unless: :skip_pundit?
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
@@ -9,6 +12,14 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
+  end
+
+  def authenticate_request
+    # p header = request.headers['Authorization']
+    p header = params[:token]
+    header = header.split(" ").last if header
+    decoded = jwt_decode(header)
+    @current_user = User.find(decoded[:user_id])
   end
 
   private
